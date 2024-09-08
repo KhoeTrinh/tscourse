6be86c747d3c2ab00fa4b8ef77e67ecdc1a65aa7
KhoeTrinh <kelvintrinh09@gmail.com>
2024-09-08 20:59:38 +0700

    type of varibles

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-function Hello () {
-    const hello = 'hello';
-    console.log(hello);
-    return 0
-}
-Hello()
+let myName: string = 'Khoa'
+myName = 'Idk'
+console.log(myName);
+
+let myNum: number = 2
+myNum += 1
+console.log(myNum);
+
+let isEasy: boolean = false
+isEasy = true
+console.log(isEasy);
